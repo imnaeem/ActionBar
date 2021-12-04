30b8891994109325bc59cb968527d80980a9a79e
imnaeem <ceinfo.pk@gmail.com>
2021-12-04 22:36:55 +0500

    created menu items

--- app/src/main/java/com/example/actionbar/MainActivity.java
+++ app/src/main/java/com/example/actionbar/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.actionbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -14,6 +19,31 @@ public class MainActivity extends AppCompatActivity {
 
          toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+                getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            case R.id.action_favorite:
+                Toast.makeText(getApplicationContext(),"Fav", Toast.LENGTH_LONG).show();
+            case R.id.action_tasks:
+                TextView textView = findViewById(R.id.textView);
+                textView.setText("Action task was clicked");
+            default:
+// If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
