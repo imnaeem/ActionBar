3c7f4fccd0ad1ac40fac2fdd5428d782c2871ebd
imnaeem <ceinfo.pk@gmail.com>
2021-12-04 22:05:59 +0500

    Custom Action Bar Added

--- app/src/main/java/com/example/actionbar/MainActivity.java
+++ app/src/main/java/com/example/actionbar/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.actionbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
 }
